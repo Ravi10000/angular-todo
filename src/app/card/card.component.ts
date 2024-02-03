2b9645f5b1d6c0fc2506943f6ced10bcce50bdfe
Ravi Sharma <ruravi80@gmail.com>
2024-02-03 21:50:18 +0530

    feat(task-list): save list in localstorage

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+type listItem = {
+  name: string;
+  createdAt: Date;
+  isFinished: boolean;
+};
 @Component({
   selector: 'app-card',
   standalone: true,
@@ -9,11 +14,26 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   // title = 'card';
   // isVisible = true;
   item = '';
-  list: string[] = [];
+  storage = localStorage.getItem('taskList');
+  list =
+    this.storage !== null
+      ? JSON.parse(this.storage)
+      : [
+          // {
+          //   name: 'Your tasks will apear here.',
+          //   createdAt: new Date(),
+          //   isFinished: false,
+          // },
+        ];
+
+  constructor() {
+    console.log({ list: this.list });
+  }
+  ngOnInit(): void {}
   // // setTitle(e: any) {
   // //   this.title = e.target.value;
   // // }
@@ -22,12 +42,24 @@ export class CardComponent {
   //   this.isVisible = !this.isVisible;
   //   return this.isVisible;
   // }
-  addToList() {
+  addToList(inputBox: any) {
     if (!this.item) return;
-    this.list.push(this.item);
+    const newTask = {
+      name: this.item,
+      createdAt: new Date(),
+      isFinished: false,
+    };
+    this.list.push(newTask);
+    localStorage.setItem('taskList', JSON.stringify(this.list));
     this.item = '';
+    inputBox.focus();
   }
   removeFromList(i: number) {
     this.list.splice(i, 1);
+    localStorage.setItem('taskList', JSON.stringify(this.list));
+  }
+  updateList() {
+    console.log({ list: this.list });
+    localStorage.setItem('taskList', JSON.stringify(this.list));
   }
 }
